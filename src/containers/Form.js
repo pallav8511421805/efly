3f89534c3c5bf87bcfe984b5c5d4519238707546
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-23 17:56:14 +0530

    change

--- src/containers/Form.js
+++ src/containers/Form.js
@@ -1,35 +1,12 @@
 import React from 'react';
-import {Button,Input,Form,Label,FormGroup} from 'reactstrap';
 
 function Form(props) {
     return (
-        <Form>
-  <FormGroup>
-    <Label for="exampleEmail">
-      Email
-    </Label>
-    <Input
-      id="exampleEmail"
-      name="email"
-      placeholder="with a placeholder"
-      type="email"
-    />
-  </FormGroup>
-  <FormGroup>
-    <Label for="examplePassword">
-      Password
-    </Label>
-    <Input
-      id="examplePassword"
-      name="password"
-      placeholder="password placeholder"
-      type="password"
-    />
-  </FormGroup>
-  <Button>
-    Submit
-  </Button>
-</Form>
+        <div className='container'>
+        <div className='main_form'>
+         <input type="text" name="name" placeholder='Enter your name'/>
+        </div>
+        </div>
     );
 }
 
