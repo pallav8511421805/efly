94fa90966a585843034f23f583b08797ac1ce71a
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-23 17:45:51 +0530

    change

--- src/containers/Form.js
+++ src/containers/Form.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import {Button,Input,Form,Label,FormGroup} from 'reactstrap';
 
 function Form(props) {
     return (
-        <div className="footer_section layout_padding">
-        <div className="container">
-            <div className="footer_logo"><a href="index.html"><img src="images/footer-logo.png" /></a></div>
-            <div className="input_bt">
-                <input type="text" className="mail_bt" placeholder="Your Email" name="Your Email" />
-                <span className="subscribe_bt" id="basic-addon2"><a href="#">Subscribe</a></span>
-            </div>
-        </div>
-    </div>
+        <Form>
+  <FormGroup>
+    <Label for="exampleEmail">
+      Email
+    </Label>
+    <Input
+      id="exampleEmail"
+      name="email"
+      placeholder="with a placeholder"
+      type="email"
+    />
+  </FormGroup>
+  <FormGroup>
+    <Label for="examplePassword">
+      Password
+    </Label>
+    <Input
+      id="examplePassword"
+      name="password"
+      placeholder="password placeholder"
+      type="password"
+    />
+  </FormGroup>
+  <Button>
+    Submit
+  </Button>
+</Form>
     );
 }
 
