654ed4f4c90ea36d9d88e37c62ab99ff33b246f5
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-23 19:03:02 +0530

    change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,22 +21,32 @@ function Footer(props) {
             alert(JSON.stringify(values, null, 2));
           },
         });
+        let {errors,handleBlur,handleSubmit,touched,values ,handleChange} = formik;
     return (
         <>
             <div className="footer_section layout_padding">
                 <div className="container">
                     <div className="footer_logo"><a href="index.html"><img src="images/footer-logo.png" /></a></div>
 
-                    <Formik>
-                        <Form>
+                    <Formik validationSchema>
+                        <Form onSubmit={handleSubmit}>
                         <div className="input_bt">
-                        <input type="email" className="mail_bt" placeholder="Your name" name="name" />
+                        <input type="text" onChange={handleChange} onBlur={handleBlur} value={values.name} className="mail_bt" placeholder="Your name" name="name" />
+                        {errors.name || touched.name ?<p style={{color:"white"}}>{errors.name}</p>: ""}
                     </div>
                     <div className="input_bt">
-                        <input type="text" className="mail_bt" placeholder="Your email" name="email" />
+                        <input type="email" 
+
+                        onChange={handleChange} onBlur={handleBlur} 
+                        value={values.email}
+                        className="mail_bt" placeholder="Your email" name="email" />
+                         {errors.email && touched.email ?<p style={{color:"white"}}>{errors.email}</p>: ""}
                     </div>
                     <div className="input_bt">
-                        <input type="password" className="mail_bt" placeholder="Your password" name="password" />
+                        <input type="password" className="mail_bt" 
+                        onChange={handleChange} onBlur={handleBlur} value={values.password}
+                        placeholder="Your password" name="password" />
+                        {errors.password && touched.password ?<p style={{color:"white"}}>{errors.password}</p>: ""}
                     </div>
                     <div style={{margin:"50px 0",textAlign:'center'}}>
                     <button style={{textAlign:"center",width:"200px",height:"40px",borderRadius:"15px",background:"rgb(242, 101, 34)",color:"white",fontSize:"20px"}} type="submit">Submit</button>
