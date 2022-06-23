38b07385016a1f0e1107938a1fa345f7398dbc07
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-06-23 18:16:00 +0530

    change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Electonic from "./containers/Electonic";
 import Fashian from "./containers/Fashian";
-import Form from "./containers/Form";
 import Home from "./containers/Home";
 import Jwellery from "./containers/Jwellery";
 function App() {
@@ -15,7 +14,6 @@ function App() {
       <Route path={"/fashian"} exact component={Fashian}/>
       <Route path={"/Electronic"} exact component={Electonic}/>
       <Route path={"/Jwellery"} exact component={Jwellery}/>
-      <Route path={"/Form"} exact component={Form}/>
     </Switch>
     <Footer/>
     </>
