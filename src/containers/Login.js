250f18acf398e6282e4bb34af0e813b65c522443
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-07-13 11:21:45 +0530

    change

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -25,7 +25,7 @@ function Login(props) {
     return (
         <div className="footer_section layout_padding">
         <div className="container">
-            <div className="footer_logo"><a href="index.html"><img src="images/footer-logo.png" /></a></div>
+            <div className="footer_logo"><p style={{color:"white",textAlign:"center",fontSize:"60px",fontWeight:"bold"}}>Login</p></div>
 
             <Formik validationSchema>
                 <Form onSubmit={handleSubmit}>
@@ -52,17 +52,6 @@ function Login(props) {
             </div>
                 </Form>
             </Formik>
-            
-            <div className="footer_menu">
-                <ul>
-                    <li><a href="#">Best Sellers</a></li>
-                    <li><a href="#">Gift Ideas</a></li>
-                    <li><a href="#">New Releases</a></li>
-                    <li><a href="#">Today's Deals</a></li>
-                    <li><a href="#">Customer Service</a></li>
-                </ul>
-            </div>
-            <div className="location_main">Help Line  Number : <a href="#">+1 1800 1200 1200</a></div>
         </div>
     </div>
     );
