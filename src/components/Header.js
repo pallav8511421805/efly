02fff79ac20cd5e4614adbc0b247375c2d573731
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-07-14 09:59:05 +0530

    change

--- src/components/Header.js
+++ src/components/Header.js
@@ -71,7 +71,7 @@ function Header(props) {
               <div className="main">
                 {/* Another variation with a button */}
                 <div className="input-group">
-                  <input type="text" className="form-control" placeholder="Search this blog" />
+                  <input type="text" className="form-control" placeholder="Search products" />
                   <div className="input-group-append">
                     <button className="btn btn-secondary" type="button" style={{ backgroundColor: '#f26522', borderColor: '#f26522' }}>
                       <i className="fa fa-search" />
