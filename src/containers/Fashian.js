8a68978f7ef595d1bb23f03ac92a2f5d2ee62889
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-07-14 11:39:46 +0530

    change

--- src/containers/Fashian.js
+++ src/containers/Fashian.js
@@ -1,147 +1,88 @@
 import React from 'react';
+import { useState } from 'react';
 
 function Fashian(props) {
-    return (
-<div className="fashion_section">
-  <div id="main_slider" className="carousel slide" data-ride="carousel">
-    <div className="carousel-inner">
-      <div className="carousel-item active">
-        <div className="container">
-          <h1 className="fashion_taital">Man &amp; Woman Fashion</h1>
-          <div className="fashion_section_2">
-            <div className="row">
-              <div className="col-lg-4 col-sm-4">
-                <div className="box_main">
-                  <h4 className="shirt_text">Man T -shirt</h4>
-                  <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
-                  <div className="tshirt_img"><img src="images/tshirt-img.png" /></div>
-                  <div className="btn_main">
-                    <div className="buy_bt"><a href="#">Buy Now</a></div>
-                    <div className="seemore_bt"><a href="#">See More</a></div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-sm-4">
-                <div className="box_main">
-                  <h4 className="shirt_text">Man -shirt</h4>
-                  <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
-                  <div className="tshirt_img"><img src="images/dress-shirt-img.png" /></div>
-                  <div className="btn_main">
-                    <div className="buy_bt"><a href="#">Buy Now</a></div>
-                    <div className="seemore_bt"><a href="#">See More</a></div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-sm-4">
-                <div className="box_main">
-                  <h4 className="shirt_text">Woman Scart</h4>
-                  <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
-                  <div className="tshirt_img"><img src="images/women-clothes-img.png" /></div>
-                  <div className="btn_main">
-                    <div className="buy_bt"><a href="#">Buy Now</a></div>
-                    <div className="seemore_bt"><a href="#">See More</a></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="carousel-item">
-        <div className="container">
-          <h1 className="fashion_taital">Man &amp; Woman Fashion</h1>
-          <div className="fashion_section_2">
-            <div className="row">
-              <div className="col-lg-4 col-sm-4">
-                <div className="box_main">
-                  <h4 className="shirt_text">Man T -shirt</h4>
-                  <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
-                  <div className="tshirt_img"><img src="images/tshirt-img.png" /></div>
-                  <div className="btn_main">
-                    <div className="buy_bt"><a href="#">Buy Now</a></div>
-                    <div className="seemore_bt"><a href="#">See More</a></div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-sm-4">
-                <div className="box_main">
-                  <h4 className="shirt_text">Man -shirt</h4>
-                  <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
-                  <div className="tshirt_img"><img src="images/dress-shirt-img.png" /></div>
-                  <div className="btn_main">
-                    <div className="buy_bt"><a href="#">Buy Now</a></div>
-                    <div className="seemore_bt"><a href="#">See More</a></div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-sm-4">
-                <div className="box_main">
-                  <h4 className="shirt_text">Woman Scart</h4>
-                  <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
-                  <div className="tshirt_img"><img src="images/women-clothes-img.png" /></div>
-                  <div className="btn_main">
-                    <div className="buy_bt"><a href="#">Buy Now</a></div>
-                    <div className="seemore_bt"><a href="#">See More</a></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="carousel-item">
-        <div className="container">
-          <h1 className="fashion_taital">Man &amp; Woman Fashion</h1>
-          <div className="fashion_section_2">
-            <div className="row">
-              <div className="col-lg-4 col-sm-4">
-                <div className="box_main">
-                  <h4 className="shirt_text">Man T -shirt</h4>
-                  <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
-                  <div className="tshirt_img"><img src="images/tshirt-img.png" /></div>
-                  <div className="btn_main">
-                    <div className="buy_bt"><a href="#">Buy Now</a></div>
-                    <div className="seemore_bt"><a href="#">See More</a></div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-sm-4">
-                <div className="box_main">
-                  <h4 className="shirt_text">Man -shirt</h4>
-                  <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
-                  <div className="tshirt_img"><img src="images/dress-shirt-img.png" /></div>
-                  <div className="btn_main">
-                    <div className="buy_bt"><a href="#">Buy Now</a></div>
-                    <div className="seemore_bt"><a href="#">See More</a></div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-sm-4">
-                <div className="box_main">
-                  <h4 className="shirt_text">Woman Scart</h4>
-                  <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
-                  <div className="tshirt_img"><img src="images/women-clothes-img.png" /></div>
-                  <div className="btn_main">
-                    <div className="buy_bt"><a href="#">Buy Now</a></div>
-                    <div className="seemore_bt"><a href="#">See More</a></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <a className="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
-      <i className="fa fa-angle-left" />
-    </a>
-    <a className="carousel-control-next" href="#main_slider" role="button" data-slide="next">
-      <i className="fa fa-angle-right" />
-    </a>
-  </div>
-</div>
+  const [fdata, setfdata] = useState([]);
+  const fashionpagedata = [
+    {
+      title: "Men's Regular Polo T-Shirt",
+      tag: "Price ",
+      price: "30",
+      id: 1,
+      img: "images/tshirt-img.png"
+    },
+    {
+      title: "Men's Regular Fit Casual Shirt",
+      tag: "Price ",
+      price: "30",
+      id: 2,
+      img: "images/dress-shirt-img.png"
+    },
+    {
+      title: "Women's Floral Maxi Dress",
+      tag: "Price ",
+      price: "30",
+      id: 3,
+      img: "images/women-clothes-img.png"
+    }
+  ];
 
-    );
+  const handlesearch = () => {
+    let search = document.getElementById("fashion_s");
+    let search_val = search.value;
+    let search_fashiondata = fashionpagedata.filter((f) => (
+      // f.title.toLowerCase().includes(search_val) 
+      // ||
+      f.tag.toLowerCase().includes(search_val) ||
+      f.price.toString().includes(search_val)
+    ))
+
+    setfdata(search_fashiondata);
+    console.log(search_fashiondata);
+  }
+
+  const filterf_data = fdata.length > 0 ? fdata : fashionpagedata ;
+
+  return (
+    <>
+      <div className="fashion_section">
+        <div className="container">
+          <h1 className="fashion_taital">Man &amp; Woman Fashion</h1>
+
+          <div className="input-group justify-content-end">
+            <input type="text" className="form-control col-lg-6 col-sm-12" id='fashion_s' placeholder="Search fashion" />
+            <button className="btn btn-secondary shadow-none" type="button" onClick={() => handlesearch()} style={{ backgroundColor: '#f26522', borderColor: '#f26522' }}><i className="fa fa-search" /></button>
+          </div>
+
+          <div className="fashion_section_2">
+            <div className="row">
+              {
+                filterf_data.map((pd) => {
+                  console.log(pd);
+                  return (
+                    <>
+                      <div className="col-lg-4 col-sm-6" key={pd.id.toString()}>
+                        <div className="box_main">
+                          <h4 className="shirt_text">{pd.title}</h4>
+                          <p className="price_text">{pd.tag}<span style={{ color: '#262626' }}>${pd.price}</span></p>
+                          <div className="tshirt_img"><img src={pd.img} /></div>
+                          <div className="btn_main">
+                            <div className="buy_bt"><a href="#">Buy Now</a></div>
+                            <div className="seemore_bt"><a href="#">See More</a></div>
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  )
+                })
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+
+  );
 }
 
 export default Fashian;
