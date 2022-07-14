0b9b3e9c8c8a757d8a08a4f5adf38c79b1b01183
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-07-14 13:08:12 +0530

    change

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -30,44 +30,43 @@ function Home(props) {
       tag: "Start Price ",
       price: "100",
       id: 4,
-      img: "images/laptop-img.png" 
+      img: "images/laptop-img.png"
     },
     {
-      title: "Laptop",
+      title: "Mobile",
       tag: "Start Price ",
       price: "100",
       id: 5,
-      img: "images/laptop-img.png" 
+      img: "images/mobile-img.png"
     },
     {
-      title: "Laptop",
+      title: "Computers",
       tag: "Start Price ",
       price: "100",
       id: 6,
-      img: "images/laptop-img.png" 
+      img: "images/computer-img.png"
     },
     {
-      title: "Laptop",
+      title: "Jumkas",
       tag: "Start Price ",
       price: "100",
       id: 7,
-      img: "images/laptop-img.png" 
+      img: "images/jhumka-img.png"
     },
     {
-      title: "Laptop",
+      title: "Necklaces",
       tag: "Start Price ",
       price: "100",
       id: 8,
-      img: "images/laptop-img.png" 
+      img: "images/neklesh-img.png"
     },
     {
-      title: "Laptop",
+      title: "Kangans",
       tag: "Start Price ",
       price: "100",
       id: 9,
-      img: "images/laptop-img.png" 
+      img: "images/kangan-img.png"
     }
-
   ];
 
   const handlesearch = (val) => {
@@ -80,8 +79,6 @@ function Home(props) {
   const filterf_data = fdata.length > 0 ? fdata : fashionpagedata;
   return (
     <>
-      {/* banner bg main end */}
-      {/* fashion section start */}
       <div className="fashion_section">
         <div className="container">
 
@@ -114,147 +111,6 @@ function Home(props) {
           </div>
         </div>
       </div>
-      {/* <div className="jewellery_section">
-        <div id="jewellery_main_slider" className="carousel slide" data-ride="carousel">
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <div className="container">
-                <h1 className="fashion_taital">Jewellery Accessories</h1>
-                <div className="fashion_section_2">
-                  <div className="row">
-                    <div className="col-lg-4 col-sm-4">
-                      <div className="box_main">
-                        <h4 className="shirt_text">Jumkas</h4>
-                        <p className="price_text">Start Price  <span style={{ color: '#262626' }}>$ 100</span></p>
-                        <div className="jewellery_img"><img src="images/jhumka-img.png" /></div>
-                        <div className="btn_main">
-                          <div className="buy_bt"><a href="#">Buy Now</a></div>
-                          <div className="seemore_bt"><a href="#">See More</a></div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-sm-4">
-                      <div className="box_main">
-                        <h4 className="shirt_text">Necklaces</h4>
-                        <p className="price_text">Start Price  <span style={{ color: '#262626' }}>$ 100</span></p>
-                        <div className="jewellery_img"><img src="images/neklesh-img.png" /></div>
-                        <div className="btn_main">
-                          <div className="buy_bt"><a href="#">Buy Now</a></div>
-                          <div className="seemore_bt"><a href="#">See More</a></div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-sm-4">
-                      <div className="box_main">
-                        <h4 className="shirt_text">Kangans</h4>
-                        <p className="price_text">Start Price  <span style={{ color: '#262626' }}>$ 100</span></p>
-                        <div className="jewellery_img"><img src="images/kangan-img.png" /></div>
-                        <div className="btn_main">
-                          <div className="buy_bt"><a href="#">Buy Now</a></div>
-                          <div className="seemore_bt"><a href="#">See More</a></div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <div className="container">
-                <h1 className="fashion_taital">Jewellery Accessories</h1>
-                <div className="fashion_section_2">
-                  <div className="row">
-                    <div className="col-lg-4 col-sm-4">
-                      <div className="box_main">
-                        <h4 className="shirt_text">Jumkas</h4>
-                        <p className="price_text">Start Price  <span style={{ color: '#262626' }}>$ 100</span></p>
-                        <div className="jewellery_img"><img src="images/jhumka-img.png" /></div>
-                        <div className="btn_main">
-                          <div className="buy_bt"><a href="#">Buy Now</a></div>
-                          <div className="seemore_bt"><a href="#">See More</a></div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-sm-4">
-                      <div className="box_main">
-                        <h4 className="shirt_text">Necklaces</h4>
-                        <p className="price_text">Start Price  <span style={{ color: '#262626' }}>$ 100</span></p>
-                        <div className="jewellery_img"><img src="images/neklesh-img.png" /></div>
-                        <div className="btn_main">
-                          <div className="buy_bt"><a href="#">Buy Now</a></div>
-                          <div className="seemore_bt"><a href="#">See More</a></div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-sm-4">
-                      <div className="box_main">
-                        <h4 className="shirt_text">Kangans</h4>
-                        <p className="price_text">Start Price  <span style={{ color: '#262626' }}>$ 100</span></p>
-                        <div className="jewellery_img"><img src="images/kangan-img.png" /></div>
-                        <div className="btn_main">
-                          <div className="buy_bt"><a href="#">Buy Now</a></div>
-                          <div className="seemore_bt"><a href="#">See More</a></div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <div className="container">
-                <h1 className="fashion_taital">Jewellery Accessories</h1>
-                <div className="fashion_section_2">
-                  <div className="row">
-                    <div className="col-lg-4 col-sm-4">
-                      <div className="box_main">
-                        <h4 className="shirt_text">Jumkas</h4>
-                        <p className="price_text">Start Price  <span style={{ color: '#262626' }}>$ 100</span></p>
-                        <div className="jewellery_img"><img src="images/jhumka-img.png" /></div>
-                        <div className="btn_main">
-                          <div className="buy_bt"><a href="#">Buy Now</a></div>
-                          <div className="seemore_bt"><a href="#">See More</a></div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-sm-4">
-                      <div className="box_main">
-                        <h4 className="shirt_text">Necklaces</h4>
-                        <p className="price_text">Start Price  <span style={{ color: '#262626' }}>$ 100</span></p>
-                        <div className="jewellery_img"><img src="images/neklesh-img.png" /></div>
-                        <div className="btn_main">
-                          <div className="buy_bt"><a href="#">Buy Now</a></div>
-                          <div className="seemore_bt"><a href="#">See More</a></div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4 col-sm-4">
-                      <div className="box_main">
-                        <h4 className="shirt_text">Kangans</h4>
-                        <p className="price_text">Start Price  <span style={{ color: '#262626' }}>$ 100</span></p>
-                        <div className="jewellery_img"><img src="images/kangan-img.png" /></div>
-                        <div className="btn_main">
-                          <div className="buy_bt"><a href="#">Buy Now</a></div>
-                          <div className="seemore_bt"><a href="#">See More</a></div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <a className="carousel-control-prev" href="#jewellery_main_slider" role="button" data-slide="prev">
-            <i className="fa fa-angle-left" />
-          </a>
-          <a className="carousel-control-next" href="#jewellery_main_slider" role="button" data-slide="next">
-            <i className="fa fa-angle-right" />
-          </a>
-          <div className="loader_main">
-            <div className="loader" />
-          </div>
-        </div>
-      </div> */}
     </>
   );
 }
